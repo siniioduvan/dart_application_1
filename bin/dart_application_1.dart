b5f0edf2f715333c6ec9d65014b95c383c8b6834
siniioduvan <sinii.oduvan@gmail.com>
2022-04-17 10:26:52 +0400

    17.0

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,4 +1,15 @@
 void main(List<String> args) {
-  var a = 5;
+  //Урок 1. Переменные.
+
+  String a = 'asd';
+  int b = 4;
+  bool c = true;
+  double d = 4.5;
+
+  final int q = 65; //нельзя изменять, получит значение при запуске
+  const w = 65; //нельзя изменять, получит значение при компиляции
+
+  //===========математика=============
+
   print(a.runtimeType);
 }
