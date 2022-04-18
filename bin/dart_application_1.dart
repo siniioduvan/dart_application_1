3d4f66887407ecc7f3834046fdc067c2e75e9456
siniioduvan <sinii.oduvan@gmail.com>
2022-04-18 10:10:58 +0400

    18.0

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,3 +1,35 @@
-void main(List<String> args) {
-  //=====Урок 2. Функции.=====
+// myAge() {
+//   return 32;
+// }
+
+int sum(int a, int b) {
+  final result = a + b;
+  return result;
 }
+
+final a = 5; //переменная созданная в глобальной области видимости
+
+void main(List<String> args) {
+  // final age = myAge();
+  // print(age);
+  final a = 4;
+  print(a); // переменная созданная внутри функции main
+
+  int sum2(int a, int b) {
+    final result = a + b;
+    return result;
+  }
+
+  // final result = sum(4, 7);
+  // print(result);
+}
+
+
+//=====Области видимости=====
+/*
+  Global             //всё что написано в глобальной функции видно отовсюду
+    fun main         //всё что написано внутри фигурных скобок ограничивает область видимости
+      fun sum2
+    sum
+
+*/
